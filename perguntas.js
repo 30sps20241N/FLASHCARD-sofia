975e9e9c2133364af34fd998033d347c6cc7c418
30sps20241N <sofia.parisotto.silva@escola.pr.gov.br>
2025-05-25 20:53:50 +0000

    aula 8(problema)

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+    'EMPRESA',
+    'Quem fundou a Disney?',
+    'Walter Elias Disney, mais conhecido como Walt Disney fundou a empresa Disney junto de seu irmão Roy Disney, no ano de 1923.'
+)
+criaCartao(
+    'PERSONAGENS',
+    'Quando Mickey Mouse foi criado?',
+    'Mickey Mouse foi criado em 1928, seu primeiro filme foi Streamboat Willie ("O Vapor Willie"), um curta metragem em preto e branco, sendo este também o 1° filme assinado por Walt.'
+)
+criaCartao(
+    'PARQUES',
+    'Qual o significado do nome do parque Magic Kingdom?',
+    'É o ponto principal da Disney, conta como atração central o castelo da Cinderela. Seu nome reflete a atmosfera de contos de fada que caracteriza o parque.'
+)
