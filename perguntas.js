5aa8b46658eef9444b7decd74bce3b7838fae315
30sps20241N <sofia.parisotto.silva@escola.pr.gov.br>
2025-05-26 19:43:43 +0000

    commit final

--- perguntas.js
+++ perguntas.js
@@ -13,3 +13,48 @@ criaCartao(
     'Qual o significado do nome do parque Magic Kingdom?',
     'É o ponto principal da Disney, conta como atração central o castelo da Cinderela. Seu nome reflete a atmosfera de contos de fada que caracteriza o parque.'
 )
+criaCartao(
+    'PERSONAGENS',
+    'Como a Fera de "A Bela e a Fera" foi inventada?',
+    'A Fera foi inspirada em cinco animais diferentes para que Bela se apaixonasse por ele: búfalo, leão, javali, gorila, urso, lobo e ser humano.'
+)
+criaCartao(
+    'PARQUES',
+    'Como os Doritos foram criados na Disney?',
+    'Após a criação da Casa de Fritos em 1955, muitas tortillas eram descartadas e então o cozinheiro teve a ideia de recortá-las em triângulos e vendê-las, e assim nasceram os "pedacinhos de ouro".'
+)
+criaCartao(
+    'EMPRESA',
+    'Quantos funcionários trabalham na Disney?',
+    'A empresa Walt Disney World, em Orlando, é o maior local empregador em um único local nos EUA, conta com mais de 62.000 funcionários.'
+)
+criaCartao(
+    'PARQUES',
+    'É possível casar-se na Disney?',
+    'Sim, a Disney oferece diversas opções de cerimônias e festas de casamento, incluindo cerimônias mais íntimas em hotéis ou jardins, ou até mesmo cerimônias tradicionais nos espaços dos parques, como o castelo da Cinderela ou o East Plaza Garden.'
+)
+criaCartao(
+    'PERSONAGENS',
+    'Qual o tamanho do cabelo da Rapunzel?',
+    'O cabelo da princesa tem 21 metros de comprimento, e totaliza 100 mil tranças individuais.'
+)
+criaCartao(
+    'EMPRESA',
+    'Por que os gatos são soltos durante a noite?',
+    'Nos anos de abertura da Disney, os gatos desfilavam pelo parque pegando os ratos que ficavam por ali, desde então esse costume permaneceu, sendo assim, durante o dia os gatos descansam em estações especiais de alimentação, e durante a noite são soltos para limparem o terreno.'
+)
+criaCartao(
+    'PERSONAGENS',
+    'Como os personagens de "Divertidamente" foram criados?',
+    'Alegria é uma estrela, Tristeza é uma lágrima, Raiva é um tijolo, Medo é um nervo exposto e Nojinho é um brócolis. A Ansiedade não tem nariz, representando a dificuldade de respirar ao ter uma crise de ansiedade, a Inveja tem olhos grandes e o Tédio é representado como um vazio.'
+)
+criaCartao(
+    'EMPRESA',
+    'Como foi a participação da Disney na Segunda Guerra Mundial?',
+    'Sua participação foi financiada pelo governo dos EUA, os vídeos produzidos tinham como propósito a propaganda e incentivo ao treinamento militar, 90% dos funcionários chegaram a fazer parte de filmes relacionados à guerra, assim como personagens famosos protagonizaram essas produções.'
+)
+criaCartao(
+    'PARQUES',
+    'A Disney é apenas um conjunto parques de atrações?',
+    'Na verdade, a Disney é muito além do que apenas um símbolo de diversão, é uma cidade planeada, com seus próprios sistemas de transporte, segurança e serviços, que dão forma a atmosfera mágica e encantadora. '
+)
